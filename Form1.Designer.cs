2a5e24d714787628e2f1f58f0386af40e1d83866
Jason Nguyen <thuong.nguyen.dev12@gmail.com>
2023-06-02 16:13:36 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace data_bosch
+{
+    partial class PCBInjection
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.status = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblPos1 = new System.Windows.Forms.Label();
+            this.lblPos2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(232, 153);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 73);
+            this.label1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(232, 261);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 73);
+            this.label2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label3.Location = new System.Drawing.Point(9, 261);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(201, 73);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "vị trí 2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label4.Location = new System.Drawing.Point(9, 153);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(201, 73);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "vị trí 1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label5.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.label5.Location = new System.Drawing.Point(376, 153);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(138, 73);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "mm";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label6.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.label6.Location = new System.Drawing.Point(376, 261);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(138, 73);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "mm";
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.status.Location = new System.Drawing.Point(768, 9);
+            this.status.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(82, 44);
+            this.status.TabIndex = 0;
+            this.status.Text = "???";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 53);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(701, 80);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyUp);
+            // 
+            // lblPos1
+            // 
+            this.lblPos1.AutoSize = true;
+            this.lblPos1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPos1.Location = new System.Drawing.Point(519, 153);
+            this.lblPos1.Name = "lblPos1";
+            this.lblPos1.Size = new System.Drawing.Size(0, 73);
+            this.lblPos1.TabIndex = 10;
+            // 
+            // lblPos2
+            // 
+            this.lblPos2.AutoSize = true;
+            this.lblPos2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPos2.Location = new System.Drawing.Point(519, 261);
+            this.lblPos2.Name = "lblPos2";
+            this.lblPos2.Size = new System.Drawing.Size(0, 73);
+            this.lblPos2.TabIndex = 11;
+            // 
+            // PCBInjection
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1027, 503);
+            this.Controls.Add(this.lblPos2);
+            this.Controls.Add(this.lblPos1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.status);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "PCBInjection";
+            this.Text = "PCBInjection";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label status;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblPos1;
+        private System.Windows.Forms.Label lblPos2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Melsec;
+
+namespace data_bosch
+{
+    public partial class PCBInjection : Form, INotifyPropertyChanged
+    {
+        FX3U plc = new FX3U("com5",9600,7,System.IO.Ports.Parity.Even,System.IO.Ports.StopBits.One);
+        string HU { get; set; } = "";
+        float pos1 { get; set; } = 0;
+        float pos2 { get; set; } = 0;
+        double demen1 { get; set; } =  0;
+        double demen2 { get; set; } =  0;
+        public double HiLimit { get; private set; }
+        public double LoLimit { get; private set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        PropertyChangedEventArgs PropertyChangedEventArgs;
+        public PCBInjection()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (plc.Open()) 
+            {
+                status.Text=("connect");
+                Timer timer= new Timer();
+            }
+            else
+            {
+                status.Text = ("disconnect");
+            }
+
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            plc.Close();
+        }
+
+        private async void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            
+            if (e.KeyCode == Keys.Enter)
+            {
+                lblPos1.ForeColor=Color.Black; lblPos2.ForeColor=Color.Black;
+                lblPos1.Text = "Done";
+                lblPos2.Text = "Done";
+                HiLimit = 7.22;
+                LoLimit = 7.0;
+                plc.WriteDWord(62, 0);
+                HU = textBox1.Text;
+                pos1 = 0;
+                label1.Text = pos1.ToString("#.##");
+                pos2 = 0;
+                label2.Text = pos2.ToString("#.##");
+                pos1 = 0; 
+                pos2 = 0;
+                demen1 = 0;
+                demen2 = 0;
+                textBox1.Text = "";
+                textBox1.Enabled = false;
+                textBox1.BackColor = Color.Gray;
+                lblPos1.Text = "Processing";
+                lblPos2.Text = "Processing";
+
+                PropertyChangedEventArgs = new PropertyChangedEventArgs(nameof(demen1));
+
+                plc.WriteDWord(60, 1);
+                do
+                {
+                    Debug.WriteLine("Processing");
+                    pos1 = plc.ReadFloat(12);
+                    label1.Text = pos1.ToString("#.##");
+                    pos2 = plc.ReadFloat(6);
+                    label2.Text = pos2.ToString("#.##");
+                    Debug.WriteLine(pos1);
+                    Debug.WriteLine(pos2);
+                    await Task.Delay(1000);
+                }
+                while (pos1==0||pos2==0);
+
+                demen1 = pos1;
+                demen2 = pos2;
+                lblPos1.Text = demen1 < HiLimit ? "Done": "Fail";
+                lblPos1.ForeColor = demen1 < HiLimit ? Color.Green : Color.Red;
+                lblPos2.Text = demen2 < HiLimit ? "Done": "Fail";
+                lblPos2.ForeColor = demen2 < HiLimit ? Color.Green : Color.Red;
+
+                PropertyChangedEventArgs = new PropertyChangedEventArgs(nameof(demen1));
+
+                plc.WriteDWord(60, 0);
+                plc.WriteDWord(62, 1);
+                textBox1.Text = "";
+                textBox1.Enabled = true;
+                textBox1.BackColor = Color.White;
+                textBox1.Focus();
+            }
+        }
+
+
+    }
+}
