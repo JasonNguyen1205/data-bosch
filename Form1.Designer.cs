121986c831e0cd4e669cca6f7077f0ccc41fb218
Vincent Pham <vincent_pham@friwo.local>
2023-06-07 09:09:46 +0700

    update New UI & Connect API

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,32 +34,36 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.status = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.txtBarcode = new System.Windows.Forms.TextBox();
             this.lblPos1 = new System.Windows.Forms.Label();
             this.lblPos2 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cbxCom = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label1.Location = new System.Drawing.Point(232, 153);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(340, 168);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 73);
+            this.label1.Size = new System.Drawing.Size(0, 55);
             this.label1.TabIndex = 3;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label2.Location = new System.Drawing.Point(232, 261);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(340, 273);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(0, 73);
+            this.label2.Size = new System.Drawing.Size(0, 55);
             this.label2.TabIndex = 4;
             // 
             // label3
@@ -70,9 +74,9 @@
             this.label3.Location = new System.Drawing.Point(9, 261);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(201, 73);
+            this.label3.Size = new System.Drawing.Size(314, 73);
             this.label3.TabIndex = 5;
-            this.label3.Text = "vị trí 2";
+            this.label3.Text = "Position 2";
             // 
             // label4
             // 
@@ -82,16 +86,16 @@
             this.label4.Location = new System.Drawing.Point(9, 153);
             this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(201, 73);
+            this.label4.Size = new System.Drawing.Size(314, 73);
             this.label4.TabIndex = 6;
-            this.label4.Text = "vị trí 1";
+            this.label4.Text = "Position 1";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.label5.ForeColor = System.Drawing.SystemColors.WindowFrame;
-            this.label5.Location = new System.Drawing.Point(376, 153);
+            this.label5.Location = new System.Drawing.Point(566, 150);
             this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(138, 73);
@@ -103,67 +107,105 @@
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.label6.ForeColor = System.Drawing.SystemColors.WindowFrame;
-            this.label6.Location = new System.Drawing.Point(376, 261);
+            this.label6.Location = new System.Drawing.Point(566, 258);
             this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(138, 73);
             this.label6.TabIndex = 8;
             this.label6.Text = "mm";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
             // 
-            // status
+            // lblStatus
             // 
-            this.status.AutoSize = true;
-            this.status.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.status.Location = new System.Drawing.Point(768, 9);
-            this.status.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.status.Name = "status";
-            this.status.Size = new System.Drawing.Size(82, 44);
-            this.status.TabIndex = 0;
-            this.status.Text = "???";
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lblStatus.Location = new System.Drawing.Point(732, 53);
+            this.lblStatus.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(82, 44);
+            this.lblStatus.TabIndex = 0;
+            this.lblStatus.Text = "???";
             // 
-            // textBox1
+            // txtBarcode
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(12, 53);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(701, 80);
-            this.textBox1.TabIndex = 9;
-            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyUp);
+            this.txtBarcode.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtBarcode.Font = new System.Drawing.Font("Arial Narrow", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBarcode.Location = new System.Drawing.Point(12, 53);
+            this.txtBarcode.Name = "txtBarcode";
+            this.txtBarcode.Size = new System.Drawing.Size(701, 50);
+            this.txtBarcode.TabIndex = 9;
+            this.txtBarcode.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyUp);
             // 
             // lblPos1
             // 
             this.lblPos1.AutoSize = true;
+            this.lblPos1.BackColor = System.Drawing.SystemColors.Control;
             this.lblPos1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPos1.Location = new System.Drawing.Point(519, 153);
+            this.lblPos1.Location = new System.Drawing.Point(727, 153);
             this.lblPos1.Name = "lblPos1";
-            this.lblPos1.Size = new System.Drawing.Size(0, 73);
+            this.lblPos1.Size = new System.Drawing.Size(320, 73);
             this.lblPos1.TabIndex = 10;
+            this.lblPos1.Text = "????????";
+            this.lblPos1.Click += new System.EventHandler(this.lblPos1_Click);
             // 
             // lblPos2
             // 
             this.lblPos2.AutoSize = true;
             this.lblPos2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPos2.Location = new System.Drawing.Point(519, 261);
+            this.lblPos2.Location = new System.Drawing.Point(727, 261);
             this.lblPos2.Name = "lblPos2";
-            this.lblPos2.Size = new System.Drawing.Size(0, 73);
+            this.lblPos2.Size = new System.Drawing.Size(320, 73);
             this.lblPos2.TabIndex = 11;
+            this.lblPos2.Text = "????????";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial Black", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(12, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(100, 27);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Barcode";
+            // 
+            // cbxCom
+            // 
+            this.cbxCom.FormattingEnabled = true;
+            this.cbxCom.Location = new System.Drawing.Point(427, 14);
+            this.cbxCom.Name = "cbxCom";
+            this.cbxCom.Size = new System.Drawing.Size(121, 21);
+            this.cbxCom.TabIndex = 13;
+            this.cbxCom.SelectedIndexChanged += new System.EventHandler(this.cbxCom_SelectedIndexChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(328, 12);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(93, 23);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "COM Port";
             // 
             // PCBInjection
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.ClientSize = new System.Drawing.Size(1027, 503);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1299, 635);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.cbxCom);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.lblPos2);
             this.Controls.Add(this.lblPos1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtBarcode);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.status);
+            this.Controls.Add(this.lblStatus);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "PCBInjection";
             this.Text = "PCBInjection";
@@ -181,10 +223,13 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label status;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.TextBox txtBarcode;
         private System.Windows.Forms.Label lblPos1;
         private System.Windows.Forms.Label lblPos2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox cbxCom;
+        private System.Windows.Forms.Label label8;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -5,8 +5,11 @@ using System.Data;
 using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Design;
+using System.IO.Ports;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Melsec;
@@ -15,15 +18,15 @@ namespace data_bosch
 {
     public partial class PCBInjection : Form, INotifyPropertyChanged
     {
-        FX3U plc = new FX3U("com5",9600,7,System.IO.Ports.Parity.Even,System.IO.Ports.StopBits.One);
+        FX3U plc = new FX3U("com5", 9600, 7, System.IO.Ports.Parity.Even, System.IO.Ports.StopBits.One);
         string HU { get; set; } = "";
         float pos1 { get; set; } = 0;
         float pos2 { get; set; } = 0;
-        double demen1 { get; set; } =  0;
-        double demen2 { get; set; } =  0;
+        double demen1 { get; set; } = 0;
+        double demen2 { get; set; } = 0;
         public double HiLimit { get; private set; }
         public double LoLimit { get; private set; }
-
+        private static Regex re2 = new Regex("^\\d{8}([-])\\d{7}([-])\\d{7}([-])\\d{3}$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
         public event PropertyChangedEventHandler PropertyChanged;
         PropertyChangedEventArgs PropertyChangedEventArgs;
         public PCBInjection()
@@ -33,21 +36,21 @@ namespace data_bosch
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            if (plc.Open()) 
+            cbxCom.Items.Clear();
+            List<String> tList = new List<String>();
+            foreach (string s in SerialPort.GetPortNames())
             {
-                status.Text=("connect");
-                Timer timer= new Timer();
+                tList.Add(s);
             }
-            else
-            {
-                status.Text = ("disconnect");
-            }
-
+            tList.Sort();
+            
+            cbxCom.Items.AddRange(tList.ToArray());
+            txtBarcode.Enabled = false;
         }
 
         private void Timer_Tick(object sender, EventArgs e)
         {
-         
+
         }
 
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
@@ -57,30 +60,44 @@ namespace data_bosch
 
         private async void textBox1_KeyUp(object sender, KeyEventArgs e)
         {
-            
+
             if (e.KeyCode == Keys.Enter)
             {
-                lblPos1.ForeColor=Color.Black; lblPos2.ForeColor=Color.Black;
-                lblPos1.Text = "Done";
-                lblPos2.Text = "Done";
-                HiLimit = 7.22;
+                lblStatus.Text = "";
+                var checkBarcode = false;
+                txtBarcode.Enabled = false;
+                txtBarcode.BackColor = Color.Gray;
+                checkBarcode = re2.IsMatch(txtBarcode.Text);
+                if (checkBarcode==false)
+                {
+                    lblStatus.Text = "Wrong Barcode!";
+                    txtBarcode.Text = "";
+                    txtBarcode.Enabled = true;
+                    txtBarcode.BackColor = Color.White;
+                    txtBarcode.Focus();
+                    plc.WriteDWord(60, 0);
+                    plc.WriteDWord(62, 1);
+                    return;
+
+                }
+                lblPos1.ForeColor = Color.Black; lblPos2.ForeColor = Color.Black;
+                //lblPos1.Text = "Done";
+                //lblPos2.Text = "Done";
+                HiLimit = 7.8;
                 LoLimit = 7.0;
                 plc.WriteDWord(62, 0);
-                HU = textBox1.Text;
+                HU = txtBarcode.Text;
                 pos1 = 0;
-                label1.Text = pos1.ToString("#.##");
+                label1.Text = pos1.ToString("#.###");
                 pos2 = 0;
-                label2.Text = pos2.ToString("#.##");
-                pos1 = 0; 
+                label2.Text = pos2.ToString("#.###");
+                pos1 = 0;
                 pos2 = 0;
                 demen1 = 0;
                 demen2 = 0;
-                textBox1.Text = "";
-                textBox1.Enabled = false;
-                textBox1.BackColor = Color.Gray;
                 lblPos1.Text = "Processing";
                 lblPos2.Text = "Processing";
-
+                string apiStatus = "Fail";
                 PropertyChangedEventArgs = new PropertyChangedEventArgs(nameof(demen1));
 
                 plc.WriteDWord(60, 1);
@@ -88,33 +105,114 @@ namespace data_bosch
                 {
                     Debug.WriteLine("Processing");
                     pos1 = plc.ReadFloat(12);
-                    label1.Text = pos1.ToString("#.##");
+                    label1.Text = pos1.ToString("#.###");
                     pos2 = plc.ReadFloat(6);
-                    label2.Text = pos2.ToString("#.##");
+                    label2.Text= pos2.ToString("#.###");
                     Debug.WriteLine(pos1);
                     Debug.WriteLine(pos2);
-                    await Task.Delay(1000);
+                    await Task.Delay(10);
                 }
-                while (pos1==0||pos2==0);
+                while (pos1 == 0 || pos2 == 0);
 
                 demen1 = pos1;
                 demen2 = pos2;
-                lblPos1.Text = demen1 < HiLimit ? "Done": "Fail";
+                lblPos1.Text =  demen1 < HiLimit ? "Pass" : "Fail";
                 lblPos1.ForeColor = demen1 < HiLimit ? Color.Green : Color.Red;
-                lblPos2.Text = demen2 < HiLimit ? "Done": "Fail";
+                if (lblPos1.Text.Equals("Pass")) {
+                    lblPos1.Text = demen1 >= LoLimit ? "Pass" : "Fail";
+                    lblPos1.ForeColor = demen1 >= LoLimit ? Color.Green : Color.Red;
+                }
+                lblPos2.Text = demen2 < HiLimit ? "Pass" : "Fail";
                 lblPos2.ForeColor = demen2 < HiLimit ? Color.Green : Color.Red;
-
-                PropertyChangedEventArgs = new PropertyChangedEventArgs(nameof(demen1));
-
+                if (lblPos2.Text.Equals("Pass")) {
+                    lblPos2.Text = demen2 >= LoLimit ? "Pass" : "Fail";
+                    lblPos2.ForeColor = demen2 >= LoLimit ? Color.Green : Color.Red;
+                }
                 plc.WriteDWord(60, 0);
                 plc.WriteDWord(62, 1);
-                textBox1.Text = "";
-                textBox1.Enabled = true;
-                textBox1.BackColor = Color.White;
-                textBox1.Focus();
+                if (lblPos1.Text.Equals("Pass") && lblPos2.Text.Equals("Pass"))
+                {
+                    apiStatus = await InsertHeightCheckData(txtBarcode.Text, 1, System.Environment.MachineName, HiLimit + "-" + demen1.ToString("#.###") + "-" + demen2.ToString("#.###"));
+                }
+                else {
+                    apiStatus = await InsertHeightCheckData(txtBarcode.Text, 0, System.Environment.MachineName, HiLimit + "-" + demen1.ToString("#.###") + "-" + demen2.ToString("#.###"));
+                }
+                if (apiStatus.Equals("OK")) {
+                    lblStatus.Text = "Insert Success!";
+                    lblStatus.ForeColor = Color.Green;
+                }
+                else
+                { 
+                    lblStatus.Text = "FAIL!!!";
+                    lblStatus.ForeColor = Color.Red;
+                }
+                PropertyChangedEventArgs = new PropertyChangedEventArgs(nameof(demen1));
+
+                
+                txtBarcode.Text = "";
+                txtBarcode.Enabled = true;
+                txtBarcode.BackColor = Color.White;
+                txtBarcode.Focus();
             }
         }
 
+        HttpClient _httpClient;
+        private async Task<string> InsertHeightCheckData(string barcode, int status, string machine, string resultTest)
+        {
+            _httpClient = new HttpClient();
+            string apiStatus = "Fail";
+            var rq = new HttpRequestMessage();
+            rq.Method = HttpMethod.Post;
+            var requestStr = $"http://fvn-s-web01:5000/api/ProcessLock/FA/InsertPCBHeightCheckAsync/" + barcode.ToString() + "/" + status + "/" + machine + "/" + resultTest;
+            Debug.WriteLine(requestStr);
+            rq.RequestUri = new Uri(requestStr);
+            var rs = await _httpClient.SendAsync(rq);
+            if (rs.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                apiStatus = "OK";
+            }
+            else {
+                apiStatus = "Fail";
+            }
+            return apiStatus;
+        }
 
+        private void lblPos1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbxCom_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            plc = new FX3U(cbxCom.SelectedItem.ToString(), 9600, 7, System.IO.Ports.Parity.Even, System.IO.Ports.StopBits.One);
+            lblPos1.Text = string.Empty;
+            lblPos2.Text = string.Empty;
+            cbxCom.Enabled = false;
+            if (plc.Open())
+            {
+                lblStatus.Text = ("Connected PLC");
+                Timer timer = new Timer();
+                txtBarcode.Enabled = true;
+                txtBarcode.BackColor = Color.White;
+                txtBarcode.Focus();
+                //try {
+                //    plc.WriteDWord(60, 0);
+                //    plc.WriteDWord(62, 1);
+                //} catch (Exception ex) {
+                //    lblStatus.Text = ("Can't Connected PLC");
+                //    txtBarcode.Enabled = false;
+                //}
+            }
+            else
+            {
+                lblStatus.Text = ("Can't Connected PLC");
+                txtBarcode.Enabled = false;
+            }
+        }
     }
 }
